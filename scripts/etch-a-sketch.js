f83f4d1861276b64d8cf9ba7f023b3bf3a7c1804
EvanOliver0 <ewoliver@protonmail.com>
2019-09-02 12:01:56 -0400

    Add functionality to clear current pad and create new pad of n by n divs

--- scripts/etch-a-sketch.js
+++ scripts/etch-a-sketch.js
@@ -1,12 +1,43 @@
+function clearCurrentPad(pad) {
+    while (pad.hasChildNodes()) {
+        pad.removeChild(pad.lastChild);
+    }
+}
+function createNewPad(pad, dimensions) {
+    pad.style.gridTemplate = `repeat(${dimensions}, 1fr) / repeat(${dimensions}, 1fr)`
+    for (let i=0; i < dimensions**2; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.addEventListener("mouseenter", mouseEnter);
+        pad.appendChild(newDiv);
+    }
+}
 function mouseEnter(e) {
     let divEntered = e.target;
     console.log(`Entered div ${divEntered.textContent}`)
     divEntered.classList.add("colored");
 }
-let container = document.querySelector("#container");
-for (let i=0; i < 256; i++) {
-    let newDiv = document.createElement("div");
-    newDiv.addEventListener("mouseenter", mouseEnter);
-    newDiv.textContent = i;
-    container.appendChild(newDiv);
+function promptForDimensions() {
+    let tries = 0;
+    let message = "How many squares per side should your new pad have?"
+    while (tries < 5) {
+        let dimensions = +prompt(message);
+        if (dimensions > 0) {
+            return dimensions;
+        }
+        else {
+            message = "Invalid input./n/nHow many squares per side should your new pad have?"
+            tries++;
+        }
+    }
+    alert("No valid input; resetting to 16x16")
+    return 16;
 }
+function resetPad(pad) {
+    let numSquares = promptForDimensions();
+    clearCurrentPad(pad);
+    createNewPad(pad, numSquares);
+}
+let pad = document.querySelector("#container");
+createNewPad(pad, 16);
+let button = document.querySelector("button");
+button.addEventListener("click", function(e) {resetPad(pad)} );
